1ec0ec32fec51286eadca29c8d8f892334d924b8
FrancisKan2 <82062089+FrancisKan2@users.noreply.github.com>
2021-04-19 02:56:13 +0100

    Add files via upload

---
+++ Cours.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeCalculator
+{
+   public class Cours
+    {
+
+        public string titre;
+        public int numeroCours, code;
+        public string Titre { get; set; }
+        public int Code { get; set; }
+        public int NumeroCours { get; set; }
+        public Cours(int numeroCours, int code, string titre)
+        {
+            this.NumeroCours = numeroCours;
+            this.Code = code;
+            this.Titre = titre;
+        }
+        public string ToString()
+        {
+            return "  *Numero de cours: " + this.NumeroCours 
+                + "  *Code: " + this.Code + 
+                "  *Titre: " + this.Titre + "\n";
+        }
+    }
+}
---
+++ Etudiant.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeCalculator
+{
+    public class Etudiant
+
+    {
+
+        public string nom, prenom;
+        public int numeroEtudiant;
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public int NumeroEtudiant { get; set; }
+        public Etudiant(int numeroEtudiant, string nom, string prenom)
+        {
+            this.NumeroEtudiant = numeroEtudiant;
+            this.Nom = nom;
+            this.Prenom = prenom;
+        }
+        public string ToString()
+        {
+            return "  *Nom: " + this.Nom 
+                + "   *Prenom: " + this.Prenom + 
+                "  *NumeroEtudiant: " + this.NumeroEtudiant + "\n";
+        }
+    }
+}
